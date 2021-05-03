cbe88ef161e6df6d0978ad5747dd239296ae2f4c
Hanna Skeppe <hannaskeppe@gmail.com>
2021-05-03 22:34:28 +0200

    Fixed typo in function.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -39,9 +39,9 @@ const openFullscreen = () => {
   if (video.requestFullscreen) {
     video.requestFullscreen();
   } else if (video.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
+    video.webkitRequestFullscreen();
   } else if (video.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
+    video.msRequestFullscreen();
   }
 }
 
